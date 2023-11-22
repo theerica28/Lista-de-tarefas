2fb096a1d766ed282967cb0b782b6baf2eba304c
Érica Santos <104101478+theerica28@users.noreply.github.com>
2023-11-22 12:30:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,33 @@
     'use strict';
 
     var ul = document.querySelector('ul');
+    var lis = ul.querySelector('li');
     var txtTask = document.querySelector('#txtTask');
     var btn = document.querySelector('#btn');
 
-    btn.addEventListener('click',function(){
-        var task = '<li>'+ txtTask.value + '</li>';
-        ul.innerHTML+= task;
+
+    btn.addEventListener('click',addTask);
+    txtTask.addEventListener('keyup',function(e){
+        if(e.keyCode === 13) {
+        addTask();
+        }
+    });
+
+    for (var i  = 0; i < lis.clientHeight; i++) {
+    lis[i].addEventListener('click',toggleDone)
+    }
+
+    function toggleDone(){
+        this.classList.toggle('done');
+
+    }
+
+    function addTask(){
+        var task = '<li>' + txtTask.value + '</li>';
+        ul.innerHTML += task;
+    
         txtTask.value = '';
         txtTask.focus();
-    })
+    }
 
-})()
+    })()
